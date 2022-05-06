b4adf0612e3aacbebd387bb84131696505efc1a6
morbi <elaedebichi@gmail.com>
2022-05-06 12:00:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { ReactComponent as ClockIcon } from './icons/clock.svg';
+import { ReactComponent as CalenderIcon } from './icons/calendar.svg';
+
+function App() {
+	const [dateState, setDateState] = useState(new Date());
+	useEffect(() => {
+		setInterval(() => {
+			//console.log('watching');
+			setDateState(new Date());
+		}, 500);
+	}, []);
+	return (
+		<div className="App">
+			<CalenderIcon />
+			<p>
+				{' '}
+				{dateState.toLocaleDateString('en-GB', {
+					day: 'numeric',
+					month: 'short',
+					year: 'numeric',
+				})}
+			</p>
+			<ClockIcon />
+			<p>
+				{dateState.toLocaleString('en-US', {
+					hour: 'numeric',
+					minute: 'numeric',
+					second: 'numeric',
+					hour12: true,
+				})}
+			</p>
+		</div>
+	);
+}
+
+export default App;
